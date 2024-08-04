482a9b2cab34f2df0ea9c1c0790b90895901d6f9
Denis9622 <152106321+Denis9622@users.noreply.github.com>
2024-08-05 00:17:39 +0300

    fix2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers } from 'redux';
+
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-// Настройка persist
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
 
 // Комбинирование редьюсеров
-const rootReducer = combineReducers({
-  contacts: persistReducer(persistConfig, contactsReducer),
+const rootReducer = {
+  contacts: contactsReducer,
   filters: filtersReducer,
-});
+};
 
 // Настройка Redux store с редьюсерами
 const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
-      serializableCheck: {
-        // Игнорируем предупреждения о несериализуемых значениях в действиях
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-        ignoredPaths: ['register'],
-      },
+      serializableCheck: false,
     }),
 });
-const persistor = persistStore(store);
 
-export { store, persistor };
+
+export default store;
